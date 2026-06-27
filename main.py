2fd535163dbb97cd9529f35ab2af856224820617
alihantangul <tangulalihan@gmail.com>
2026-06-28 00:06:56 +0300

    initial weather ETL

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+import os
+
+API_KEY = os.getenv("8c42d73b9b4c1c4b557d25bf427495ec")
+
+cities = ["Istanbul", "Ankara", "Izmir", "Berlin", "London"]
+
+def extract(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    return requests.get(url).json()
+
+def transform(data):
+    return {
+        "city": data["name"],
+        "temp": data["main"]["temp"],
+        "humidity": data["main"]["humidity"],
+        "description": data["weather"][0]["description"]
+    }
+
+rows = []
+
+for c in cities:
+    data = extract(c)
+
+    if "main" in data:
+        rows.append(transform(data))
+
+df = pd.DataFrame(rows)
+df.to_csv("weather.csv", index=False, encoding="utf-8-sig")
+
+print(df)
