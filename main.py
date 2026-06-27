e7892ecdbf4b7dc8ca1a1833759becf8bc931658
alihantangul <tangulalihan@gmail.com>
2026-06-28 00:39:25 +0300

    appending weather data

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from datetime import datetime, timezone
 API_KEY = os.getenv("API_KEY")
 
 if not API_KEY:
-    raise ValueError("API_KEY secret bulunamadı. GitHub Secrets içinde API_KEY tanımlanmış olmalı.")
+    raise ValueError("API_KEY bulunamadı. GitHub Secrets içinde API_KEY olmalı.")
 
 cities = ["Istanbul", "Ankara", "Izmir", "Berlin", "London"]
 
+FILENAME = "weather.csv"
+
+
 def extract(city):
     url = "https://api.openweathermap.org/data/2.5/weather"
+
     params = {
         "q": city,
         "appid": API_KEY,
@@ -24,24 +28,44 @@ def extract(city):
     print(f"{city} response:", response.text)
 
     response.raise_for_status()
+
     return response.json()
 
-def transform(data):
+
+def transform(data, timestamp):
     return {
-        "timestamp_utc": datetime.now(timezone.utc).isoformat(),
+        "timestamp_utc": timestamp,
         "city": data["name"],
         "temp": data["main"]["temp"],
         "humidity": data["main"]["humidity"],
         "description": data["weather"][0]["description"]
     }
 
+
 rows = []
+
+run_timestamp = datetime.now(timezone.utc).isoformat()
 
 for city in cities:
     data = extract(city)
-    rows.append(transform(data))
+    rows.append(transform(data, run_timestamp))
 
-df = pd.DataFrame(rows)
-df.to_csv("weather.csv", index=False, encoding="utf-8-sig")
 
-print(df)
+new_df = pd.DataFrame(rows)
+
+# Eğer weather.csv daha önce varsa eski veriyi oku
+if os.path.exists(FILENAME):
+    old_df = pd.read_csv(FILENAME)
+
+    # Eski veri + yeni veri birleşir
+    final_df = pd.concat([old_df, new_df], ignore_index=True)
+
+else:
+    # İlk çalışmaysa sadece yeni veri yazılır
+    final_df = new_df
+
+
+# CSV'yi güncel haliyle tekrar kaydet
+final_df.to_csv(FILENAME, index=False, encoding="utf-8-sig")
+
+print(final_df)
